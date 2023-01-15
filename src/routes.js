bc8f7bc6bfb89d91b08a52060dc281bdf9304c34
azanniel <leo.azannielttt@gmail.com>
2023-01-15 17:51:40 -0400

    👔 add logic to update, remove and search user

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,30 @@
 import { randomUUID } from 'node:crypto';
 import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database;
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users');
+      const { search } = req.query;
+
+      const users = database.select('users', search && {
+        name: search,
+        email: search
+      });
+
       return res.end(JSON.stringify(users));
     }
   },
 
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const { name, email } = request.body;
+      const { name, email } = req.body;
 
       const user = {
         id: randomUUID(),
@@ -27,7 +34,35 @@ export const routes = [
 
       database.insert('users', user);
 
-      return response.writeHead(201).end();
+      return res.writeHead(201).end();
+    }
+  },
+
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { name, email } = req.body;
+
+      database.update('users', id, {
+        name,
+        email
+      });
+
+      return res.writeHead(204).end();
+    }
+  },
+
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete('users', id);
+
+      return res.writeHead(204).end();
     }
   }
 ]
